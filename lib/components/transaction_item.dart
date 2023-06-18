c14f7ebe802e4c98fd21d113f4c326c990dc26b4
Thiago Duarte <thiago.duarte.q@gmail.com>
2023-06-18 10:33:33 -0300

    refactor: Adicionado Chaves no Listview

---
+++ lib/components/transaction_item.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatefulWidget {
+  final Transaction tr;
+  final void Function(String p1) onRemove;
+
+  const TransactionItem({
+    super.key,
+    required this.tr,
+    required this.onRemove,
+  });
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  static const colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.purple,
+    Colors.orange,
+    Colors.green,
+  ];
+
+  Color? _backgroundColor;
+
+  @override
+  void initState() {
+    super.initState();
+    int i = widget.tr.id.hashCode % colors.length;
+    _backgroundColor = colors[i];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: _backgroundColor,
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text('R\$${widget.tr.value}'),
+            ),
+          ),
+        ),
+        title: Text(
+          widget.tr.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        subtitle: Text(
+          DateFormat('d MMM y').format(widget.tr.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 480
+            ? TextButton(
+                onPressed: () => widget.onRemove(widget.tr.id),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.delete,
+                      color: Theme.of(context).colorScheme.error,
+                    ),
+                    Text(
+                      'Excluir',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).colorScheme.error,
+                onPressed: () => widget.onRemove(widget.tr.id),
+              ),
+      ),
+    );
+  }
+}
